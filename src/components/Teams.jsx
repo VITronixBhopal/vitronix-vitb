1630c0028fe623fa061ba88515a4b4ea06870513
VITronixBhopal <140756385+VITronixBhopal@users.noreply.github.com>
2024-08-06 22:33:45 +0530

    Update Teams.jsx

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -8,7 +8,7 @@ import { fadeIn } from '../utils/motion'
 
 const MemberCard= ({children,key,photo,...props})=> (
   <motion.div {...props} variants={fadeIn("up","spring",key*0.2,0.75)} className='relative xxs:max-w-[180px] md:max-w-[250px] overflow-hidden rounded-2xl shadow-lg group'>
-    <img src={photo} alt='key' className='transition-transform group-hover:scale-110 duration-200 w-auto xxs:h-[160px] md:h-[280px]'/>
+    <img src={photo} alt='key' className='transition-transform group-hover:scale-110 duration-200 w-auto xxs:h-[160px] xxs:w-[160px] md:h-[280px] md:h-[280px]'/>
     <div className='absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
     <div className='p-4 text-white'>{children}</div>
     </div>
@@ -28,7 +28,7 @@ const Teams = () => {
       </h2></div>
     </motion.div>
     <div className='py-10 bg-[#1d1836] bg-opacity-90'>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <div className='flex flex-row flex-wrap justify-center xxs:gap-2 md:gap-10'>
         {members.map((member,index)=>(
           <MemberCard key={index} photo={member.photo}>
             <h3 className='text-white font-montserrat text-[18px]'>{member.name}</h3>
