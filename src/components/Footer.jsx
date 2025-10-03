49b2be2e97a94936dd186e960c4c5c83913982ca
pr13260 <princeraj13260@gmail.com>
2025-10-03 22:18:34 +0530

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,18 @@ const Footer = () => {
   return (
     <footer className='footer'>
     <div class="bg-transparent socialIcons">
-        <a href="https://www.instagram.com/vitronix_vitb?igsh=czNzYTgzaGdiejJ0" className="icon instagram" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/vitronix_vitb" className="icon instagram" target="_blank" rel="noopener noreferrer">
           <FaInstagram />
         </a>
         <a href="https://chat.whatsapp.com/DvnZp6zlxjyEdXdJa8zGpY" className="icon whatsapp" target="_blank" rel="noopener noreferrer">
           <FaWhatsapp />
         </a>
-        <a href="https://www.linkedin.com/company/the-vitronix-club/mycompany/" className="icon linkedin" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/company/the-vitronix-club" className="icon linkedin" target="_blank" rel="noopener noreferrer">
           <FaLinkedinIn />
         </a>
     </div>
     <div className="footerBottom bg-inherit">
-        <p>Copyright &copy;2024 | Designed by | <span class="designer">VITronix Club</span> </p>
+        <p>Copyright &copy;2025 | Designed by | <span class="designer">VITronix Club</span> </p>
         <p>VIT Bhopal</p>
     </div>
     </footer>
